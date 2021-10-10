ad05ddb25e0ad3bddf6498677b04fa29cbb88ac8
Arnav Choudhury <arnav13@gmail.com>
2021-10-10 13:36:15 +0530

    Creating APIKey model instances at dj-stripe app load time in its ready method

--- djstripe/apps.py
+++ djstripe/apps.py
@@ -4,7 +4,31 @@ dj-stripe - Django + Stripe Made Easy
 import pkg_resources
 from django.apps import AppConfig
 
+# from django.db.models.signals import post_migrate
+
 __version__ = pkg_resources.get_distribution("dj-stripe").version
+
+
+# todo refactor this pr into 2 PRs, a separate one for creating apikey instances and the current one only for fixing djstripe_owner_account field
+# todo add a test
+# def get_or_create_stripe_keys(**kwargs):
+#     """Create Stripe public, secret, and restricted keys for both the modes if provided. If they don't exist already"""
+#     from django.conf import settings
+
+#     from djstripe.models import APIKey
+
+#     dict = {
+#         "STRIPE_SECRET_KEY": getattr(settings, "STRIPE_SECRET_KEY", ""),
+#         "STRIPE_TEST_SECRET_KEY": getattr(settings, "STRIPE_TEST_SECRET_KEY", ""),
+#         "STRIPE_LIVE_SECRET_KEY": getattr(settings, "STRIPE_LIVE_SECRET_KEY", ""),
+#         "STRIPE_PUBLIC_KEY": getattr(settings, "STRIPE_PUBLIC_KEY", ""),
+#         "STRIPE_LIVE_PUBLIC_KEY": getattr(settings, "STRIPE_LIVE_PUBLIC_KEY", ""),
+#         "STRIPE_TEST_PUBLIC_KEY": getattr(settings, "STRIPE_TEST_PUBLIC_KEY", ""),
+#     }
+
+#     for value in dict.values():
+#         if value:
+#             APIKey.objects.get_or_create_by_api_key(value)
 
 
 class DjstripeAppConfig(AppConfig):
@@ -31,3 +55,10 @@ class DjstripeAppConfig(AppConfig):
             version=__version__,
             url="https://github.com/dj-stripe/dj-stripe",
         )
+
+        # # todo import and run this methid somewhere else as doing it in ready is not good pratice.
+        # # create stripe keys if they don't exist already
+        # # we do not use the post_migrate signal because the user can change the keys whenever they want.
+        # post_migrate.connect(get_or_create_stripe_keys, sender=self)
+        # print(self.get_model("APIKey"))
+        # # todo add a method to create the keys in case migrations are not run, need to check that the apikey model exists for that.
